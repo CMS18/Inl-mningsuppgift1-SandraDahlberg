125f5b9224f37e3f4e5c30b8a54b2efadfc5eda1
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-11-13 12:52:03 +0100

    Add project files.

---
+++ SandrasBank.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SandrasBank.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
