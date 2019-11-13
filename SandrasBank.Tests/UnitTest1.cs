232fda732074baae6c3b6e3081e4c3cf54072e05
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2019-11-13 15:03:32 +0100

    Added transaction functionality

--- SandrasBank.Tests/UnitTest1.cs
+++ SandrasBank.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SandrasBank.Web.Models;
+using System;
+using System.Linq;
 
 namespace SandrasBank.Tests
 {
@@ -6,8 +9,42 @@ namespace SandrasBank.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDeposit()
         {
+            // Arrange
+            var bank = new BankRepository();
+            var account = bank.Accounts.SingleOrDefault(a => a.AcccountId == 1);
+            var balanceBefore = account.Balance;
+
+            // Act
+            bank.Deposit(100m, account.AcccountId);
+
+            // Assert
+            Assert.AreEqual((balanceBefore + 100), account.Balance);
+        }
+
+        [TestMethod]
+        public void TestWithdraw()
+        {
+            // Arrange
+            var bank = new BankRepository();
+            var account = bank.Accounts.SingleOrDefault(a => a.AcccountId == 1);
+            var balanceBefore = account.Balance;
+
+            // Act
+            bank.Withdraw(100m, account.AcccountId);
+
+            // Assert
+            Assert.AreEqual((balanceBefore - 100), account.Balance);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestAmountToHigh()
+        {
+            var bank = new BankRepository();
+            bank.Withdraw(1000000000000m, 1);
+
         }
     }
 }
